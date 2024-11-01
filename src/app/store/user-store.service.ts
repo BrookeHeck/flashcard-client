05a4ca114554f099230c87d18fe9771a73f10916
BrookeHeck <b19heck@gmail.com>
2024-11-01 13:55:31 -0400

    add store to store logged in user data

---
+++ src/app/store/user-store.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable} from "rxjs";
+import User from "../model/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserStoreService {
+  private _user$: BehaviorSubject<User> = new BehaviorSubject<User>(null);
+  public user$: Observable<User> = this._user$.asObservable();
+}
