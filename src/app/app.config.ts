341daa71bc1fe16cb549cd699147fa5a126e1bce
BrookeHeck <b19heck@gmail.com>
2024-11-01 13:44:46 -0400

    add authorization header with token to all requests that are not login or register

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,17 +3,21 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import {provideHttpClient} from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {JwtModule} from "@auth0/angular-jwt";
+import {authInterceptor} from "./interceptor/auth.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(),
-  importProvidersFrom(JwtModule.forRoot({
-    config: {
-      tokenGetter: tokenGetter,
-      allowedDomains: ["localhost:8080"]
-    }
-  }))
+  providers: [provideRouter(routes), provideClientHydration(),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
+    importProvidersFrom(JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ["localhost:8080"]
+      }
+    }))
   ]
 };
 
--- src/app/interceptor/auth.interceptor.ts
+++ src/app/interceptor/auth.interceptor.ts
@@ -1,8 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject} from "@angular/core";
+import {AuthenticationService} from "../service/authentication.service";
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthenticationService);
   if(req.url.includes('user/login') || req.url.includes('user/register')) {
     return next(req);
   }
-  return next(req);
+  authService.loadToken();
+  const header: string = `Bearer ${authService.getToken()}`;
+  const request = req.clone({setHeaders: {Authorization: header} });
+  return next(request);
 };
