2b6c6d8cf5e4da3f1ef78f5b32c2af76893f7ae6
BrookeHeck <b19heck@gmail.com>
2024-11-25 04:29:38 -0500

    add service and component for showing organizations

---
+++ src/app/components/super/organizations-overview/organizations-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizations-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './organizations-overview.component.html',
+  styleUrl: './organizations-overview.component.scss'
+})
+export class OrganizationsOverviewComponent {
+
+}
