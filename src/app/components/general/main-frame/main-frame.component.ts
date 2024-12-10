fdb23575d3816b1e4411dcd9db366b8fa99b5afa
BrookeHeck <b19heck@gmail.com>
2024-12-10 02:46:38 -0500

    add a placeholder for the footer with no content for now

--- src/app/components/general/main-frame/main-frame.component.ts
+++ src/app/components/general/main-frame/main-frame.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {ToolbarComponent} from "../toolbar/toolbar.component";
 import {RouterOutlet} from "@angular/router";
 import {ToastModule} from "primeng/toast";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-main-frame',
@@ -9,7 +10,8 @@ import {ToastModule} from "primeng/toast";
   imports: [
     ToolbarComponent,
     RouterOutlet,
-    ToastModule
+    ToastModule,
+    FooterComponent
   ],
   templateUrl: './main-frame.component.html',
   styleUrl: './main-frame.component.scss'
