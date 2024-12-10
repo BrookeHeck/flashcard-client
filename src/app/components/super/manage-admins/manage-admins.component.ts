3d91bc1ac623ee03f3f5c95016ef2f422e7f9286
BrookeHeck <b19heck@gmail.com>
2024-12-10 03:08:33 -0500

    add method for getting organization admins and use it in the manage admins component

--- src/app/components/super/manage-admins/manage-admins.component.ts
+++ src/app/components/super/manage-admins/manage-admins.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {Observable, switchMap, take} from "rxjs";
+import Role from "../../../model/role";
+import {UserRolesService} from "../../../service/user-roles.service";
+import {ActivatedRoute} from "@angular/router";
+import {ROLE} from "../../../enum/ROLE";
 
 @Component({
   selector: 'app-manage-admins',
@@ -7,6 +12,15 @@ import { Component } from '@angular/core';
   templateUrl: './manage-admins.component.html',
   styleUrl: './manage-admins.component.scss'
 })
-export class ManageAdminsComponent {
+export class ManageAdminsComponent implements OnInit {
+  organizationAdminList: Observable<Role[]>;
+  userRoleService = inject(UserRolesService);
+  route = inject(ActivatedRoute);
+
+  ngOnInit() {
+    this.organizationAdminList = this.route.paramMap.pipe(
+      switchMap(params => this.userRoleService.getAllUsersForRoleAtOrg(params['organizationId'], ROLE.ADMIN)),
+    );
+  }
 
 }
--- src/app/service/user-roles.service.ts
+++ src/app/service/user-roles.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpRequestService} from "./http-request.service";
+import {ROLE} from "../enum/ROLE";
+import Role from "../model/role";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +11,8 @@ export class UserRolesService {
 
   constructor(private http: HttpRequestService) { }
 
-
+  getAllUsersForRoleAtOrg(orgId: number, role: ROLE): Observable<Role[]> {
+    return this.http.getRequest<Role[]>(`role/get-all-role-users-organization/${orgId}/${role}`);
+  }
 
 }
